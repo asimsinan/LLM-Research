f01db5baa548fb5dd0ff4253b49a879cc83223f2
asimsinan <asimyuksel@sdu.edu.tr>
2024-01-22 21:05:07 +0300

    refactor

--- src/add_new_paper.py
+++ src/add_new_paper.py
@@ -53,11 +53,9 @@ def init_llm():
     )
     return summarizer
 summarizer = init_llm()
-
-
 def summarize(abstract):
     output = summarizer(abstract)
-    if (len(abstract) >50):
+    if (len(abstract) > 50):
         summary = output[0]['summary_text']
         bullet_points = summary.split(". ")
         markdown = f"### Bullet Points\n\n"
@@ -66,13 +64,11 @@ def summarize(abstract):
         return markdown
 
 def rename_file(filename):
-
     new_filename = re.sub(r'^\d+\.\d+v\d+_', '', filename)
     new_filename = new_filename[new_filename.find(' ') + 1:]
 
     new_filename = re.sub(r'_', ' ', new_filename)
     os.rename(filename, new_filename)
-
 
 def add_paper_by_id(paper_id):
     client = arxiv.Client()
@@ -83,18 +79,18 @@ def add_paper_by_id(paper_id):
     directory = str(date_object.year)
     if not os.path.exists(directory):
         os.makedirs(directory)
-    subprocess.run(['getpaper','-d', directory, paper_id])
+    subprocess.run(['getpaper', '-d', directory, paper_id])
 
-    with open(directory+'/000_Paper_List.json', 'r') as f:
+    with open(directory + '/000_Paper_List.json', 'r') as f:
         paper_json = json.load(f)
 
     paper_json[paper_id]["published_date"] = formatted_date
     paper_json[paper_id]["year"] = date_object.year
-    paper_json[paper_id]["categories"]=paper.categories
-    rename_file(directory+"/"+paper_json[paper_id]["download_name"])
+    paper_json[paper_id]["categories"] = paper.categories
+    rename_file(directory + "/" + paper_json[paper_id]["download_name"])
 
-    paper=paper_json[paper_id]
-    with open(paper_id+'.md', 'w') as f:
+    paper = paper_json[paper_id]
+    with open('./src/' + paper_id + '.md', 'w') as f:
         # Write the paper title, authors, and published date
         f.write(
             f"\n\n{1}. [{paper['title']}]({paper['abs_url']}), {','.join(paper['authors'])}, {paper['published_date']}\n")
@@ -110,15 +106,11 @@ def add_paper_by_id(paper_id):
         f.write(f"     {summarize(abstract)}\n")
         print(paper["title"])
 
-    #with open('./src/papers.json', 'r') as f:
+    # with open('./src/papers.json', 'r') as f:
     #    papers = json.load(f)
     #    papers.append(paper_json)
-    #with open('./src/papers.json', 'w') as f:
+    # with open('./src/papers.json', 'w') as f:
     #    json.dump(papers, f)
 
 
-
-
-
 add_paper_by_id("2401.08577")
-
